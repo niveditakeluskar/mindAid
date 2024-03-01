71b779135c3c8a3a164c42923126eb3bc08454b5
pranali <pranali.m@d-insights.global>
2024-03-01 11:06:59 +0000

    logout model changes

--- resources/js/Pages/LayoutComponent.vue
+++ resources/js/Pages/LayoutComponent.vue
@@ -5,6 +5,27 @@
 
     <!-- Inertia's slot where individual pages will be rendered -->
     <slot :moduleId="moduleId" />
+       <template>
+    <div>
+      <!-- Your other content -->
+
+      <modal v-if="isLogoutModalVisible" @click.prevent="closeLogoutModal">
+        <p slot="header">Logout Alert</p>
+        <p>You are inactive on the screen for a few minutes. Do you really want to logout?</p>
+        <button @click.prevent="logout">Yes</button>
+        <button @click.prevent="closeLogoutModal">No</button>
+      </modal>
+    </div>
+    <div>
+      <button @click="showModal">Open Modal</button>
+
+      <Modal v-model="isModalVisible">
+        <p>Your modal content goes here.</p>
+        <button @click="closeModal">Close Modal</button>
+      </Modal>
+    </div>
+
+  </template>
 
     <!-- Footer component -->
     <Footer />
@@ -15,16 +36,37 @@
 import Header from './Header.vue';
 import Footer from './Footer.vue';
 import axios from 'axios';
+import { ref } from 'vue';
+import { Modal } from 'vue3-modal';
 
 export default {
   components: {
     Header,
     Footer,
-    axios
+    axios,
+    Modal,
+  },
+  setup() {
+    const isModalVisible = ref(false);
+
+    const showModal = () => {
+      isModalVisible.value = true;
+    };
+
+    const closeModal = () => {
+      isModalVisible.value = false;
+    };
+
+    return {
+      isModalVisible,
+      showModal,
+      closeModal,
+    };
   },
   data() {
     return {
       moduleId: null,
+      isLogoutModalVisible: false,
     };
   },
   async mounted() {
@@ -37,6 +79,8 @@ export default {
       }
       const moduleId = await this.getPageModuleID();
       this.initializeScripts(moduleId, patientId);
+      console.log("isLogoutModalVisible==>", this.isLogoutModalVisible);
+      setInterval(this.checkTimeInterval.bind(this), 1000);
     } catch (error) {
       console.error('Error fetching moduleID:', error);
     }
@@ -135,7 +179,7 @@ export default {
         })["catch"](function (error) {
           console.error(error, error.response);
         });
-        var idleInterval = setInterval(checkTimeInterval, 1000); // 1 Seconds
+        // var idleInterval = setInterval(checkTimeInterval, 1000); // 1 Seconds
         $(this).mousemove(function (e) {
           // idleTime = 0;
           localStorage.setItem("idleTime", 0);
@@ -146,50 +190,96 @@ export default {
         });
       });
 
-      var sessionIdleTime = 0; // Initialize sessionIdleTime
-      var checkTimeInterval = function timerIncrement() {
-        sessionIdleTime = localStorage.getItem("idleTime");
-        var showPopupTime = localStorage.getItem("showPopupTime"); //changes by ashvini
-        var sessionTimeoutInSeconds = localStorage.getItem("sessionTimeoutInSeconds"); //changes by ashvini
-        var systemDate = localStorage.getItem("systemDate");
-        var currentDate = new Date();
-        var res = Math.abs(Date.parse(currentDate) - Date.parse(systemDate)) / 1000;
-        var idleTime = parseInt(sessionIdleTime) + (res % 60);
-        if (idleTime >= showPopupTime) {
-          // $('#logout_modal').modal('show');   
-          var visiblemodal = $('#logout_modal').is(':visible');
-          if (visiblemodal) {
-            console.log('visiblemodal');
-          } else {
-            $('#logout_modal').modal('show');
-          }
+      // var sessionIdleTime = 0; // Initialize sessionIdleTime
+      // var checkTimeInterval = function timerIncrement() {
+      //   sessionIdleTime = localStorage.getItem("idleTime");
+      //   var showPopupTime = localStorage.getItem("showPopupTime"); //changes by ashvini
+      //   var sessionTimeoutInSeconds = localStorage.getItem("sessionTimeoutInSeconds"); //changes by ashvini
+      //   var systemDate = localStorage.getItem("systemDate");
+      //   var currentDate = new Date();
+      //   var res = Math.abs(Date.parse(currentDate) - Date.parse(systemDate)) / 1000;
+      //   var idleTime = parseInt(sessionIdleTime) + (res % 60);
+      //   if (idleTime >= showPopupTime) {
+      //     // // $('#logout_modal').modal('show');
+      //     // var visiblemodal = $('#logout_modal').is(':visible');
+      //     // if (visiblemodal) {
+      //     //   console.log('visiblemodal');
+      //     // } else {
+      //     //   $('#logout_modal').modal('show');
+      //     // }
+      //     console.log('(idleTime >= showPopupTime');
+      //     this.isLogoutModalVisible = true;
 
-          if (idleTime >= sessionTimeoutInSeconds) {
-            var visiblemodal = $('#logout_modal').is(':visible');
-            if (visiblemodal) {
-              // $('#logout_modal').modal('hide');   
-              $("#sign-out-btn")[0].click();
-              var base_url = window.location.origin;
-              // alert(base_url);  
-              window.location.href = base_url + '/rcare-login';
-              window.location.reload();
-            }
-          }
+      //     if (idleTime >= sessionTimeoutInSeconds) {
+      //       console.log('(idleTime >= sessionTimeoutInSeconds');
+      //       var visiblemodal = $('#logout_modal').is(':visible');
+      //       if (visiblemodal) {
+      //         // $('#logout_modal').modal('hide');
+      //         // $("#sign-out-btn")[0].click();
+      //         window.location.href = base_url + '/logout';
+      //         var base_url = window.location.origin;
+      //         // alert(base_url);  
+      //         window.location.href = base_url + '/rcare-login';
+      //         window.location.reload();
+      //       }
+      //     }
+      //   }
+      //   localStorage.setItem("idleTime", idleTime);
+      //   // localStorage.setItem("idleTime", 0);
+      //   localStorage.setItem("systemDate", currentDate);
+      // };
+
+      // //end of initializeScripts
+      // // $("#logout_yes").click(function (e) {
+      // //   // $("#sign-out-btn")[0].click();
+      // //   window.location.href = base_url + '/logout';
+      // // });
+
+      // // $("#logout_no").click(function (e) {
+      // //   $('#logout_modal').modal('hide');
+      // // });
+    },
+    // logout() {
+    //   window.location.href = '/logout';
+    //   this.isLogoutModalVisible = false;
+    // },
+    // closeLogoutModal() {
+    //   this.isLogoutModalVisible = false;
+    // },
+    logout() {
+      // Handle logout logic
+      // Close the modal after handling the logic
+      window.location.href = '/logout';
+      this.$modal.hide('modal-logout'); // Assuming you give the modal a name
+    },
+    closeLogoutModal() {
+      this.$modal.hide('modal-logout');
+    },
+    checkTimeInterval() {
+      // This function is defined as a method within the Vue component
+      var showPopupTime = localStorage.getItem("showPopupTime");
+      var sessionTimeoutInSeconds = localStorage.getItem("sessionTimeoutInSeconds");
+      var systemDate = localStorage.getItem("systemDate");
+      var currentDate = new Date();
+      var res = Math.abs(Date.parse(currentDate) - Date.parse(systemDate)) / 1000;
+      var idleTime = parseInt(localStorage.getItem("idleTime")) + (res % 60);
+      console.log("idleTime==>", idleTime);
+      if (idleTime >= showPopupTime) {
+        console.log('(idleTime >= showPopupTime)');
+        this.isLogoutModalVisible = true;
+        console.log("isLogoutModalVisible==>", this.isLogoutModalVisible);
+
+        if (idleTime >= sessionTimeoutInSeconds) {
+          console.log('(idleTime >= sessionTimeoutInSeconds)');
+          window.location.href = '/logout';
+          window.location.reload();
+          window.location.href = '/rcare-login';
+          window.location.reload();
         }
-        localStorage.setItem("idleTime", idleTime);
-        // localStorage.setItem("idleTime", 0);
-        localStorage.setItem("systemDate", currentDate);
-      };
-
-      //end of initializeScripts
-      $("#logout_yes").click(function (e) {
-        $("#sign-out-btn")[0].click();
-      });
-
-      $("#logout_no").click(function (e) {
-        $('#logout_modal').modal('hide');
-      });
-    }
+      }
+      localStorage.setItem("idleTime", idleTime);
+      localStorage.setItem("systemDate", currentDate);
+    },
   },
 };
 </script>
