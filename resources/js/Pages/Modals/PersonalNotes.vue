440985e36717084be85cde2e7067f4fed8f6e3a2
Nivedita Keluskar <nivedita.keluskar@d-insights.global>
2024-03-15 05:52:08 +0000

    PersonalNotes.vue null handled

--- resources/js/Pages/Modals/PersonalNotes.vue
+++ resources/js/Pages/Modals/PersonalNotes.vue
@@ -114,7 +114,7 @@ export default {
         onMounted(async () => {
             try {
                 personalnotesTime.value = document.getElementById('page_landing_times').value;
-                personal_notes_data.value = props.personal_notes_data.static.personal_notes;
+                personal_notes_data.value = props.personal_notes_data?.static?.personal_notes;
             } catch (error) {
                 console.error('Error on page load:', error);
             }
