98668dcd22b479d174ee88e24295df68b9c86768
Ashwini Mali <ashwini.m@d-insights.global>
2024-04-15 08:04:10 +0000

    practice and practice grp changes

--- packages/RCare/Org/src/OrgPackages/Practices/src/Http/Requests/practicesAddRequest.php
+++ packages/RCare/Org/src/OrgPackages/Practices/src/Http/Requests/practicesAddRequest.php
@@ -28,8 +28,10 @@ class practicesAddRequest extends FormRequest
                 'number'                => 'required|min:6|alpha_num',
                 'location'              => 'required|min:2|regex:/^[a-zA-Z0-9.-]*$/',
                 'phone'                 => 'required|max:14|phone', 
-                'key_contact'           => 'required|min:2|regex:/^[a-zA-Z0-9,.-]*$/',
-                'address'               => 'required|min:3|address',
+                // 'key_contact'           => 'required|min:2|regex:/^[a-zA-Z0-9,.-]*$/',
+                'key_contact'           => 'required|min:2|regex:/^[a-zA-Z0-9,.\s-]*$/', //for space
+                // 'address'               => 'required|min:3|address',
+                'address'               => 'required|min:3|regex:/^[a-zA-Z0-9,.\s-]*$/',
                 'outgoing_phone_number' => 'required|max:14|phone',
                 'billable'              => 'nullable|boolean',
                 'practice_group'        => 'nullable|integer',
